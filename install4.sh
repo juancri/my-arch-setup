af5863766921bec873a1acd764f27b89498077f5
JC Olivares <juancri@juancri.com>
2021-03-14 03:36:19 -0700

    Updating user setup

--- install4.sh
+++ install4.sh
@@ -9,10 +9,27 @@ makepkg -si
 
 # Install AUR packages
 pikaur -S \
-    davinci-resolve-beta \
+    alttab-git \
     google-chrome \
     polybar \
     reaper-bin \
     visual-studio-code-bin \
-    zoom
+    zoom \
+    grun
+
+# Install bash git prompt
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
+
+# Copy dot files
+echo "Copying dot files..."
+cp ./dotfiles/bashrc ~/.bashrc
+mkdir -p ~/.config/bspwm
+cp ./dotfiles/bspwmrc ~/.config/bspwm
+cp ./dotfiles/polybar ~/.config/bspwm
+cp ./dotfiles/sxhkdrc ~/.config/bspwm
+
+# Generate SSH key
+echo "Generating SSH key..."
+mkdir -p ~/.ssh
+ssh-keygen -f ~/.ssh/id_rsa -N ""
 
